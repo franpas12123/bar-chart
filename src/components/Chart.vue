391a9518fb12f3b7712d479522bf22da6f41d5b8
Frances Pasulohan <franpas12123@gmail.com>
2021-10-14 08:59:49 +0800

    add apex charts; add random number mixin

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,42 +1,145 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="">
+      <v-col class="mt-12" md="6" sm="12" cols="12" align="center" justify="center">
+        <apexchart
+          type="bar"
+          :height="options.chart.height"
+          :options="options"
+          :series="series"
+        ></apexchart>
+      </v-col>
+      <v-col class="mt-12" md="6" sm="12" cols="12" align="center" justify="center">
+        <apexchart
+          type="bar"
+          :height="options.chart.height"
+          :options="options"
+          :series="series1"
+        ></apexchart>
+      </v-col>
+      <!-- <v-spacer></v-spacer> -->
+      <!-- </v-row> -->
+      <!-- <v-row> -->
+
+      <v-col class="mt-12" md="6" sm="12" cols="12" align="center" justify="center">
+        <apexchart
+          type="bar"
+          :height="options.chart.height"
+          :options="options"
+          :series="series2"
+        ></apexchart>
+      </v-col>
+
+      <v-col class="mt-12" md="6" sm="12" cols="12" align="center" justify="center">
+        <apexchart
+          type="bar"
+          :height="options.chart.height"
+          :options="options"
+          :series="series3"
+        ></apexchart>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: 'Chart',
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  created() {
+    // this.categories.forEach(() => {
+    this.series[0].data.push(this.getRndInteger(30, 100));
+    this.series1[0].data.push(this.getRndInteger(30, 100));
+    this.series2[0].data.push(this.getRndInteger(30, 100));
+    this.series3[0].data.push(this.getRndInteger(30, 100));
+    // }
+    this.options.xaxis.categories = this.categories;
+  },
+  data() {
+    return {
+      categories: [
+        'Business plan',
+        'Marketing plan',
+        'Pro-forma financials',
+        'Pre-underwriting',
+      ],
+      options: {
+        chart: {
+          id: 'vuechart-example',
+          height: '500px',
+        },
+        xaxis: {
+          categories: [],
+        },
+        plotOptions: {
+          bar: {
+            barHeight: '100%',
+            distributed: true,
+            dataLabels: {
+              position: 'bottom',
+              enabled: false,
+            },
+          },
+        },
+        legend: {
+          position: 'right',
+          width: 200,
+          horizontalAlign: 'right',
+        },
+        colors: [
+          '#33b2df', // blue
+          '#546E7A',
+          '#A5978B',
+          '#f48024',
+          '#2b908f',
+          '#d4526e',
+          '#13d8aa',
+          '#f9a3a4',
+          '#90ee7e',
+          '#69d2e7',
+        ],
+        responsive: [
+          {
+            breakpoint: 800,
+            options: {
+              legend: {
+                position: 'bottom',
+                horizontalAlign: 'center',
+                width: '100%',
+              },
+            },
+          },
+        ],
+      },
+      series: [
+        {
+          name: '',
+          data: [],
+        },
+      ],
+      series1: [
+        {
+          name: '',
+          data: [],
+        },
+      ],
+      series2: [
+        {
+          name: '',
+          data: [],
+        },
+      ],
+      series3: [
+        {
+          name: '',
+          data: [],
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
+import Vue from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import VueApexCharts from 'vue-apexcharts';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueApexCharts);
+Vue.component('apexchart', VueApexCharts);
+
+Vue.mixin({
+  methods: {
+    getRndInteger(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+  },
+});
 
 new Vue({
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
