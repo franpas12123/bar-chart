52437ece425827c63175d7bc7b2461af823776ec
Frances Pasulohan <franpas12123@gmail.com>
2021-10-14 10:53:32 +0800

    change router paths to chartv1 and chartv2

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,28 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Chartv1 from '@/components/Chartv1.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'chartv1',
+    component: Chartv1,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/chartv2',
+    name: 'chartv2',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ '@src/components/Chartv2.vue'),
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
