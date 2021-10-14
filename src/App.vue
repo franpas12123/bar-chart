514fb2112ff37d1e30d5c96510fe4ee936f9fa69
Frances Pasulohan <franpas12123@gmail.com>
2021-10-14 10:54:31 +0800

    revert file after being changed when router was added

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,58 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar app dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="@/assets/site_logo.png"
+          transition="scale-transition"
+          width="40"
+        />
+        <strong>Black Business Alliance</strong>
+      </div>
+      <v-spacer></v-spacer>
+      <v-btn @click="$router.push('chartv1')" target="_blank" text>
+        <v-icon>mdi-poll</v-icon>
+        <span class="ml-2">V1 Chart</span>
+      </v-btn>
+      <v-btn @click="$router.push('chartv2')" target="_blank" text>
+        <v-icon>mdi-poll</v-icon>
+        <span class="ml-2">V2 Chart</span>
+      </v-btn>
+    </v-app-bar>
+    <v-main class="app">
+      <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: 'App',
+  components: {
+  },
+  data: () => ({
+    items: [
+      { title: 'Reports', icon: 'mdi-view-dashboard' },
+      { title: 'Business Directory', icon: 'mdi-image' },
+      { title: 'Contacts', icon: 'mdi-help-box' },
+    ],
+    right: null,
+  }),
+};
+</script>
+<style>
+.app {
+  background-color: #ebebeb;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.drawer {
+  color: white;
+  background-color: #363636;
+}
+.site-logo {
+  height: 35px;
+  width: auto;
+  margin-right: 4px;
 }
 </style>
