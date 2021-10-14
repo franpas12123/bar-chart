c63b21e0deb00e0e40873b50ee962b688027e6c6
Frances Pasulohan <franpas12123@gmail.com>
2021-10-14 11:28:25 +0800

    add table component

---
+++ src/components/Table.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="items"
+    :items-per-page="10"
+    class="elevation-1"
+  ></v-data-table>
+</template>
+<script>
+export default {
+  props: ['items', 'perPage'],
+  data() {
+    return {
+      headers: [
+        { text: 'Date', value: 'createdAt' },
+        { text: 'Business Name', value: 'companyName' },
+        { text: 'Owner Name', value: 'name' },
+      ],
+    };
+  },
+};
+</script>
